3a72c843a22041e74a67e3e76530d510991b416a
Marwaahmedfouad <125098112+Marwaahmedfouad@users.noreply.github.com>
2025-06-02 13:55:46 +0300

    update of labels

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -21,11 +21,12 @@ export default function ShareMealPage() {
         <form className={classes.form} action={formAction}>
           <div className={classes.row}>
             <p>
-              <label htmlFor="name">Chef's Name</label>
+              {/* <label htmlFor="name">Chef's Name</label> */}
+              <label htmlFor="name"> Name</label>
               <input type="text" id="name" name="name" required />
             </p>
             <p>
-              <label htmlFor="email">Chef's Email</label>
+              <label htmlFor="email">Email</label>
               <input type="email" id="email" name="email" required />
             </p>
           </div>
